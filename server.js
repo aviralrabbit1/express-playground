10d7a6afe17027e9398d38b18547d73929bd4a5a
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-23 02:17:56 +0530

    added ID router

--- server.js
+++ server.js
@@ -36,8 +36,21 @@ app.get('/messages', (req, res) => {
 })
 
 app.get('/friends', (req, res) => {
-    res.send(friends);
+    res.json(friends);
 })
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId); // Number() or +req.params
+    const friend = friends[friendId];
+    if(friend){
+        res.status(200).json(friend);
+    }
+    else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
+});
 
 app.post('/messages', (req, res) => {
     console.log(`Updating messages`);
