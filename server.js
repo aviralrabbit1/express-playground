69e6bc588e84952aab108bafe1c7812fa2b39b22
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-23 01:42:55 +0530

    trying get/post methods

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello Aviral!');
+})
+
+app.get('/messages', (req, res) => {
+    res.send('<ul><li>How have you been?</li></ul>');
+})
+
+app.post('/messages', (req, res) => {
+    console.log(`Updating messages`);
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`)
 });
