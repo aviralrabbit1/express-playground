ff1ca76bea2422885da24a4257e66db7981274b0
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-23 02:04:06 +0530

    trying json get method

--- server.js
+++ server.js
@@ -4,12 +4,39 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: 'Arijit Sahai'
+    },
+    {
+        id: 1,
+        name: 'Ujjwal Dhir'
+    },
+    {
+        id: 2,
+        name: 'Naveen Kumar'
+    },
+    {
+        id: 3,
+        name: 'Divyanand'
+    },
+    {
+        id: 4,
+        name: 'Pranjal Agarwal'
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello Aviral!');
 })
 
 app.get('/messages', (req, res) => {
     res.send('<ul><li>How have you been?</li></ul>');
+})
+
+app.get('/friends', (req, res) => {
+    res.send(friends);
 })
 
 app.post('/messages', (req, res) => {
