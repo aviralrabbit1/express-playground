18b733ca7ca7e2d5c2239493457abfa2ce9c26ad
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-26 20:25:13 +0530

    createed separate handlebars

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -7,7 +7,12 @@ const path = require('path');
 // names function can be tracked in errors
 function getMessages(req, res){
     const image = path.join(__dirname,'..','public', 'images', 'TEDxHBTI.jpg');
-    res.sendFile(image);
+    // res.sendFile(image);
+
+    res.render('messages', {
+        title: 'Message to my friends',
+        friend: 'Suryansh',
+    });
     // res.sendfile('../public/TEDxHBTI .jpg');
     // res.send('<ul><li>How have you been?</li></ul>');
 }
